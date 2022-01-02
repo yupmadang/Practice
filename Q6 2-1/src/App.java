436897f10e852c70e2f34bf0ba16a3fd8568190d
Yongjin Jeon <yjin1185@gmail.com>
2022-01-02 22:49:34 +0900

    Q6 2-1

---
+++ Q6 2-1/src/App.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class App {
+
+    static int CardConv(int x, int r, char[] d){
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do{
+            d[digits++] = dchar.charAt(x % r);
+            x /= r;
+
+            
+        }while(x != 0);
+        return digits;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner stdIn = new Scanner(System.in);
+        int no, cd, dno, retry;
+        char[] cno = new char[32];
+
+        System.out.println("10진수를 기수변환합니다.");
+        do{
+            do{
+                System.out.print("변환하는 음이 아닌 정수 : ");
+                no = stdIn.nextInt();
+            }while (no < 0);
+
+            do{
+                System.out.print("어떤 진수로 변환할까요? (2~36) : ");
+                cd = stdIn.nextInt();
+            }while(cd < 2 || cd > 36);
+
+            dno = CardConv(no, cd, cno);
+
+            /*System.out.print(cd + "진수로는 ");
+            for(int i = dno -1; i >= 0; i--){
+                System.out.print(cno[i]);
+            }*/
+
+            System.out.print(cd + "진수로는 "); /*배열의 앞쪽에 윗자리를 넣는 메서드*/
+            for(int i = 0; i <= dno - 1; i++){
+                System.out.print(cno[i]);
+            }
+
+            System.out.println("입니다.");
+
+            System.out.print("한번 더 할까요 (1. 예/ 0. 아니오): ");
+            retry = stdIn.nextInt();
+        }while(retry == 1);
+    }
+}
+
